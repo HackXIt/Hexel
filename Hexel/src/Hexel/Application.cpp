bc8f6efb4da2bae86365a32620588403911b38d7
HackXIt <hackxit@gmail.com>
2024-11-19 16:54:28 +0100

    Update changes for Event System video

--- Hexel/src/Hexel/Application.cpp
+++ Hexel/src/Hexel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Hexel
 {
 	Application::Application()
@@ -14,6 +17,15 @@ namespace Hexel
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HX_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HX_TRACE(e);
+		}
 		while (true);
 	}
 }
